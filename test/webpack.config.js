e5f79c296d05b29325dbb0607ef695cc35bc2d05
Crystalline Emerald <crystalline.emerald@gmail.com>
2017-02-28 00:50:08 +0300

    reproducing publicPath bug

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -4,17 +4,19 @@ module.exports = {
   entry: './entry.js',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'build')
+    path: path.join(__dirname, 'build'),
+    publicPath: 'http://localhost:8080'
   },
   module: {
     loaders: [
       {
         test: /\.font\.(js|json)$/,
-        loader: 'style!css!' + require.resolve('../')
+        //loader: 'style-loader!css-loader!' + require.resolve('../')
+        loader: 'style-loader!css-loader!webfonts-loader'
       },
       {
         test: /\.(woff|eot|ttf|svg)$/,
-        loader: 'url'
+        loader: 'url-loader'
       }
     ]
   }
